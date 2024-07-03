57c1f74cbcc2e46f8d828725b9cf6b5a013f5686
Ppvinny <vinnyseries@hotmail.com>
2024-07-02 21:21:44 -0300

    proxima aula

--- src/main/java/br/com/alura/Screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/Screenmatch/ScreenmatchApplication.java
@@ -1,8 +1,6 @@
 package br.com.alura.Screenmatch;
 
-import br.com.alura.Screenmatch.model.DadosSerie;
-import br.com.alura.Screenmatch.service.ConsumoApi;
-import br.com.alura.Screenmatch.service.ConverteDados;
+import br.com.alura.Screenmatch.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,13 +14,9 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		var consumoApi = new ConsumoApi();
-		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=da491df5");
-		//System.out.println(json);
-		//json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
-		System.out.println(json);
-		ConverteDados conversor = new ConverteDados();
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dados);
+		Principal principal = new Principal();
+		principal.exibeMenu();
+
+
 	}
 }
